121c217a05620a90aaeb410c4690afd1f14507e6
Jan Sudczak <jansudczak@gmail.com>
2018-04-11 08:34:41 +0200

    Ups, forgot about FormPrompt

---
+++ TotalCommander/FormPrompt.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TotalCommander
+{
+    partial class FormPrompt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(12, 48);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(181, 23);
+            this.buttonOK.TabIndex = 0;
+            this.buttonOK.Text = "Ok\r\n";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(199, 48);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Anuluj";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(12, 22);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(262, 20);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxInput_KeyPress);
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(13, 5);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(50, 13);
+            this.labelMessage.TabIndex = 3;
+            this.labelMessage.Text = "Message";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // FormPrompt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(294, 80);
+            this.ControlBox = false;
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormPrompt";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FormPrompt";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        public System.Windows.Forms.TextBox textBoxInput;
+    }
+}
---
+++ TotalCommander/FormPrompt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TotalCommander
+{
+    public partial class FormPrompt : Form
+    {
+        public FormPrompt(string title, string message="")
+        {
+            InitializeComponent();
+            this.Text = title;
+            this.labelMessage.Text = message;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+          
+            if (!validFolderName(textBoxInput.Text))
+            { 
+                errorProvider1.SetError(textBoxInput, "Nazwa jest pusta lub zawiera nieodpowiednie znaki");
+               // buttonOK.DialogResult = DialogResult.None;
+                return;
+            }
+
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private bool validFolderName(string text)
+        {
+            return (text != "" && text.IndexOfAny(Path.GetInvalidPathChars()) <= 0 && !text.Contains('\\') && !text.Contains('/'));       
+        }
+
+        private void textBoxInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+                buttonOK.PerformClick();
+            if (e.KeyChar == (char)Keys.Escape)
+                buttonCancel.PerformClick();
+        }
+    }
+}
